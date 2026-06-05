aa76bb35101c4de79696a82a2fbe3a79a040cd05
Blanzat <raphael.blanzat@ynov.com>
2026-06-05 10:16:39 +0200

    app.js

--- client/app.js
+++ client/app.js
@@ -1,7 +1,30 @@
 const button = document.getElementById("publishButton");
 const postInput = document.getElementById("postInput");
+const postsContainer = document.getElementById("postsContainer");
 
-button.addEventListener("click", () => {
-    console.log(postInput.value);
+
+button.addEventListener("click", async () => {
+
+    const text = postInput.value;
+
+    await fetch("/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ text })
+    });
+
+    const res = await fetch("/posts");
+    const posts = await res.json();
+
+    postsContainer.innerHTML = "";
+
+    posts.forEach(post => {
+        const div = document.createElement("div");
+        div.innerText = post.text;
+        postsContainer.appendChild(div);
+    });
+    
+    postInput.value = "";
 });
-
--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
-const express = require("express")
-
+const express = require("express");
 const app = express();
+
+app.use(express.json());
+
+let posts = [];
+
+app.get("/posts", (req, res) => {
+    res.json(posts);
+});
+
+app.post("/posts", (req, res) => {
+    posts.push(req.body);
+    res.json({ success: true });
+});
 
 app.use(express.static("client"));
 
